3e5a801c8d912af99288991cf31aeb224832e67d
SaiVamshiK <saivamshi.k.24@gmail.com>
2023-04-17 16:34:00 +0530

    typing an string in ts

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { Greet } from './components/Greet';
 import { Person } from './components/Person';
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
   const nameList = [
@@ -21,12 +22,14 @@ function App() {
   const name = {
     firstName: "KL",
     lastName: "Rahul"
-  }
+  };
+  const statusValue = "success";
   return (
     <div className="App">
       <Greet name="Vamshi" count={10} isLoggedIn={true}/>
       <Person personName = {name}/>
       <PersonList listNames={nameList}/>
+      <Status status={statusValue}/>
     </div>
   );
 }
---
+++ src/components/Status.tsx
@@ -0,0 +1,17 @@
+type StatusProps = {
+  status: string;
+};
+
+export const Status = (props: StatusProps) => {
+  return (
+    <div>
+      {props.status === "loading" ? (
+        <h2>Loading...</h2>
+      ) : props.status === "success" ? (
+        <h2>Data fetched successfully</h2>
+      ) : (
+        <h2>Error Fetching data</h2>
+      )}
+    </div>
+  );
+};
